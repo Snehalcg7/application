96cab233a4ddccf286626612ae824fbaeaca6474
Snehalcg7 <snehalcg7@gmail.com>
2025-01-21 11:55:31 +0000

    added changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Ohhh Hi');
+  res.send('Application deployment');
 });
 
 app.listen(8081, () => {
