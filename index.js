457f3afa3f5bf1015af74f9fcc137ac57ed57974
Snehalcg7 <snehalcg7@gmail.com>
2025-01-21 09:14:30 +0000

    Revert "added changes"
    This reverts commit 80b3cfd598d4eb2701b18a1fe92acfef922f86ca.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Deployment Successful!!');
+  res.send('Ohhh Hi');
 });
 
 app.listen(8081, () => {
